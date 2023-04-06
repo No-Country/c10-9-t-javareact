c3042ad14e97b287fb4f8bd23f5457bae26f9930
germanpistan <anibalgermanpistan@gmail.com>
2023-04-06 16:23:21 -0300

    app properties con db

--- demo/src/main/java/DevSkills/com/example/demo/domain/entity/AreaDeInteres.java
+++ demo/src/main/java/DevSkills/com/example/demo/domain/entity/AreaDeInteres.java
@@ -1,4 +1,16 @@
 package DevSkills.com.example.demo.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
 public class AreaDeInteres {
+
+    @Column(name = "idArea", nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idArea;
+    private String nombre;
+
 }
